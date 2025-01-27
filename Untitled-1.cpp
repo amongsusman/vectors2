20db8c30182d040d1defc382ab334d0e5ee489ba
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-01-27 10:04:50 -0500

    Update Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -6,18 +6,21 @@ Extra: Sorting a vector
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
     vector<string> languages = {"Mandarin", "English", "Spanish"};
     char resp;
     string lang;
     int pos;
+    bool run = true;
     do {
         cout << "What would you like to do?" << endl;
-        cout << "A) Print all languages\nB) Insert an element\nC) Remove an element\nD) Sort the languages" << endl;
+        cout << "A) Print all languages\nB) Insert an element\nC) Remove an element\nD) Sort the languages\nE) Leave" << endl;
         cin >> resp;
         switch (resp) {
             case 'A': {
+                // uses iterators to go through the languages vector and prints out the element at the pointer
                 for (auto it = languages.begin(); it != languages.end(); it++) {
                     cout << *it << " ";
                 }
@@ -29,23 +32,32 @@ int main() {
                 cin >> lang;
                 cout << "What position would you like to insert it at?" << endl;
                 cin >> pos;
+                // inserts the element at the user's position in the vector
                 languages.insert(languages.begin() + pos, lang);
                 break;
             }
             case 'C': {
                 cout << "What position would you like to remove?" << endl;
                 cin >> pos;
+                // removes the element at the user's wanted position
                 languages.erase(languages.begin() + pos);
                 break;
             }
             case 'D': {
+                // uses std::sort method to sort the vector
                 sort(languages.begin(), languages.end());
                 break;
             }
+            case 'E': {
+                // ends the loop
+                run = false;
+                break;
+            }
             default: {
+                // error trap if they don't enter a valid option
                 cout << "Invalid choice" << endl;
                 break;
             }
         }
-    } while
+    } while (run);
 }
